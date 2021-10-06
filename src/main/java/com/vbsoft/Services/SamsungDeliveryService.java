a1e0642c3f1ec170a4b91ea7d17e275b08586551
vb010894 <234098sam>
2021-10-06 10:23:43 +0300

    SAMSUNG API FIX

--- src/main/java/com/vbsoft/Services/SamsungDeliveryService.java
+++ src/main/java/com/vbsoft/Services/SamsungDeliveryService.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.vbsoft.Modeles.In.PKFInfo;
 import com.vbsoft.Modeles.Out.ACKANSD.ACKANSDelivery;
 import com.vbsoft.Utils.HibernateUtils;
+import okhttp3.*;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Service;
@@ -14,10 +15,14 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class SamsungDeliveryService {
 
+    private final String URL = "112.106.130.24:29443";
+    private final Logger LOG = Logger.getLogger("Root");
 
     public void saveDeliveryToFile(final PKFInfo REQUEST_BODY) throws IOException {
         XmlMapper mapper = new XmlMapper();
@@ -58,6 +63,16 @@ public class SamsungDeliveryService {
         answer.setAckSendDate(new Date());
         answer.setAckSendTime(new Date());
         answer.setInfo("SUCCESS");
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(MediaType.parse("text/xml"), mapper.writeValueAsString(answer));
+        Request request = new Request.Builder().url(URL).post(body).build();
+        try (Response response = client.newCall(request).execute()) {
+            LOG.info(response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOG.log(Level.WARNING, e.getMessage());
+        }
         return mapper.writeValueAsString(answer);
     }
 
