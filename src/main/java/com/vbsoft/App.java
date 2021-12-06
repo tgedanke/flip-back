9879ada94874d149acee7aec262f3f0c8e666855
vb010894 <234098sam>
2021-12-06 10:41:21 +0300

    SAMSUNG API. SEND REQUEST BY DOCUMENT NUMBER #23

--- src/main/java/com/vbsoft/App.java
+++ src/main/java/com/vbsoft/App.java
@@ -1,15 +1,19 @@
 package com.vbsoft;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 /**
  * Root application class.
  */
 @SpringBootApplication
-public class App 
+public class App
 {
     /**
      * Main method.
@@ -17,7 +21,9 @@ public class App
      */
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
+        System.out.println(Arrays.toString(context.getEnvironment().getActiveProfiles()));
+        System.out.println(context.getEnvironment().getProperty("spring.datasource.url"));
     }
 
 }
